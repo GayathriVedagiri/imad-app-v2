33c54a83455df4e6f719b4f98c8e8c09abba2226
GayathriVedagiri <gayuvedagiri97@gmail.com>
2017-03-16 20:19:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-
+});
 app.get('article-one',function(req,res){
   res.send("article one requested and it will be served");  
 });
