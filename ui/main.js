89ffe5f57f79b7928f5ff9433efc81c9deb8d667
GayathriVedagiri <gayuvedagiri97@gmail.com>
2017-03-19 00:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 var button=document.getElementById("button");
 var counter = 0;
-button.onclick=function(){
+
+button.onclick=function() {
+    
     counter=counter + 1;
     var span=document.getElementById("counter");
     span.InnerHTML=counter.toString();
